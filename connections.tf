81a6c7567129324d8b980827bcaf15e7696776d3
Alexey Zamora F <azamoraf@gmail.com>
2019-07-23 09:24:26 +0000

    First commit. Terraform Hello World

---
+++ connections.tf
@@ -0,0 +1,16 @@
+provider "google" {
+    credentials = "${file("../account.json")}"
+    project = "alex6-sbt"
+    region = "us-central1"
+}
+
+provider "aws" {
+    region = "us-west-2"
+}
+
+/*provider "azure" {
+    subscription_id = "0"
+    client_id = "1"
+    client_secret = "2"
+    tenant_id = "3"
+}*/
