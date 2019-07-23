0432fe543d319947c51a5a8efbd7455f8bb03a53
Alexey Zamora F <azamoraf@gmail.com>
2019-07-23 10:56:03 +0000

    New server and a subnet

--- connections.tf
+++ connections.tf
@@ -8,9 +8,9 @@ provider "aws" {
     region = "us-west-2"
 }
 
-/*provider "azure" {
+provider "azurerm" {
     subscription_id = "0"
     client_id = "1"
     client_secret = "2"
     tenant_id = "3"
-}*/
+}
